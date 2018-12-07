42d1b279b00018196f2df10a98ae05aefbddd86f
kagif101 <otnielsre@gmail.com>
2018-12-07 20:34:18 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$config = [
+"imei" => "868594040672050",
+"sign" => "1a09853340a916063fd90f16e8be480aa51ec4c2",
+"token" => "f92ca8b01d08aae796b3b940ed457536",
+"uuid" => "1e68f62c-2472-48c2-8cb0-f87fd62532dc"
+];
